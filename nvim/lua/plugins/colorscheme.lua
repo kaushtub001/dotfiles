c5cd8fcb3287a973fd1a9615d4dd7175003b8411
kaushtub001 <kumarakaushtub123@gmail.com>
2026-06-01 22:11:59 +0530

    Changes made to ghostty and nvim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,38 @@
 return {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    priority = 1000,
 
     config = function()
-      require("rose-pine").setup({
-        variant = "moon",
+      vim.g.moonflyTransparent = true
+      vim.g.moonflyCursorColor = true
+      vim.g.moonflyItalics = true
+      vim.g.moonflyNormalFloat = true
+      vim.g.moonflyTerminalColors = true
+      vim.g.moonflyUndercurls = true
 
-        styles = {
-          transparency = true,
-        },
-      })
+      vim.cmd.colorscheme("moonfly")
 
-      vim.cmd.colorscheme("rose-pine")
+      local hl = vim.api.nvim_set_hl
+
+      hl(0, "Normal", { bg = "NONE" })
+      hl(0, "NormalNC", { bg = "NONE" })
+
+      hl(0, "SignColumn", { bg = "NONE" })
+      hl(0, "EndOfBuffer", { bg = "NONE" })
+
+      hl(0, "NeoTreeNormal", { bg = "NONE" })
+      hl(0, "NeoTreeNormalNC", { bg = "NONE" })
+
+      hl(0, "NormalFloat", { bg = "NONE" })
+      hl(0, "FloatBorder", { bg = "NONE" })
+
+      hl(0, "TelescopeNormal", { bg = "NONE" })
+      hl(0, "TelescopeBorder", { bg = "NONE" })
+
+      hl(0, "StatusLine", { bg = "NONE" })
+      hl(0, "StatusLineNC", { bg = "NONE" })
     end,
   },
 }
